43eb3aaefde93f8c83acfa9ed3a917070725f020
catcarbonell <hello@catcarbn.com>
2020-05-05 17:42:16 -0700

    Added emojis to my intro

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Emoji from '../Emoji';
 import LandingNav from './LandingNav';
 import headshot from '../../assets/headshot.jpg';
 import '../../styles/DarkModeApp.scss';
@@ -14,12 +15,13 @@ const Landing = () => {
                     <img src={headshot} className="profile-img" alt="my face" />
                 </div>
                 <div>
-                    <h1>Hi, I'm <Link to="/about">Cat</Link>!</h1>
+                    <h1>Hi! I'm <Link to="/about">Cat</Link>!</h1>
                     
                     <div className="landing-description">
-                    <p className="subtitle">I <Link to="/projects">design and build</Link> web apps!</p>
-                    <p className="subtitle"> &amp; write newbie-friendly <a href="https://dev.to/catcarbn" target="new"> dev articles</a>!</p>
-                    <p className="subtitle"> &amp; <a href="https://twitch.tv/catvscode" target="new">stream</a> my attempts at coding/design challenges on <a href="https://twitch.tv/catvscode" target="new">Twitch</a>! </p>
+                    <p className="subtitle"> <Emoji symbol="💻" label="a laptop computer" /> I <Link to="/projects">design and build</Link> web apps!</p>
+                    <p className="subtitle"> <Emoji symbol="✍🏾" label="a tan hand writing with a pen" /> &amp; write newbie-friendly <a href="https://dev.to/catcarbn" target="new"> dev articles</a>!</p>
+                    <p className="subtitle"> <Emoji symbol="📺" label="a tan hand writing with a pen" /> &amp; 
+                    <a href="https://twitch.tv/catvscode" target="new">stream</a> my attempts at coding challenges on <a href="https://twitch.tv/catvscode" target="new">Twitch</a>! </p>
                     </div>
 
                     <LandingNav />
